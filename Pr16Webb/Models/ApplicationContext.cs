acc552c6e19102e69595546b2c6d00a2e18d9b8a
Re1ch <Re1ch@DESKTOP-VDMLPSS>
2022-05-19 23:42:48 +0300

    Добавьте файлы проекта.

---
+++ Pr16Webb/Models/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Pr16Webb.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Friend> Friends { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
